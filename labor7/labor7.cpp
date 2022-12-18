5433aff1960e075c3976de9013881b0d02a46dd6
AleksandrShkurkin <saha.sanek.555@gmail.com>
2022-12-18 13:25:46 +0200

    test change commit

--- labor7/labor7.cpp
+++ labor7/labor7.cpp
@@ -16,12 +16,10 @@ int main()
 
     cout << "Results: " << endl;
 
-    cout << "" << endl;
-
-    cout << setw(5) << "Y" << setw(15) << "x" << endl;
-    while (x <= max)
+    cout << setw(10) << "Y" << setw(15) << "x" << endl;
+    while (x >= max)
     {
-        Y = pow(x, 1. / 3.) + pow(x, 3.) - 3;
+        Y = pow(x, 1. / 3) + pow(x, 3.) - 3;
         cout << setw(5) << "Y = " << Y << setw(10) << "x = " << x << endl;
         x += step;
     }
@@ -29,6 +27,7 @@ int main()
     cout << "" << endl;
 
     cout << "Loop break" << endl;
+    cout << "End of the programm." << endl;
 
     system("pause");
     return 0;
