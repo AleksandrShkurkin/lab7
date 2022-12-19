8912cc0fe2d72fa07401f0053d7d589f06f79700
AleksandrShkurkin <saha.sanek.555@gmail.com>
2022-12-19 08:57:22 +0200

    test commit num2

--- labor7/labor7.cpp
+++ labor7/labor7.cpp
@@ -14,14 +14,14 @@ int main()
     cout << "Enter step: ";
     cin >> step;
 
-    cout << "Results: " << endl;
+    cout << "Results of calculations: " << endl;
 
     cout << setw(10) << "Y" << setw(15) << "x" << endl;
     while (x >= max)
     {
         Y = pow(x, 1. / 3) + pow(x, 3.) - 3;
-        cout << setw(5) << "Y = " << Y << setw(10) << "x = " << x << endl;
-        x += step;
+        cout << setw(3) << "Y = " << Y << setw(10) << "x = " << x << endl;
+        x = x + step;
     }
 
     cout << "" << endl;
